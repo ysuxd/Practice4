48f1ea4938c2f49e7790ec0a097c7193324f4eb4
Anton <ivanakunkin023@gmail.com>
2025-12-11 14:00:07 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Npgsql;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice
+{
+    public class DatabaseConnection
+    {
+        private string connectionString = "Server=localhost;Port=5432;Database=Practice;User Id =postgres;Password=";
+        public NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(connectionString);
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
